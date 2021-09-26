312f33d4bf82056c0b221c3e908655b03399857a
agmmnn <agmyak66@gmail.com>
2021-09-26 11:35:34 +0300

    Add: theme module

---
+++ polydown/theme.py
@@ -0,0 +1,21 @@
+t_file = "[grey11 on dark_khaki]📁:[/grey11 on dark_khaki]"
+t_img = "[grey11 on dark_khaki]🖼️:[/grey11 on dark_khaki]"
+
+t_skipped_file = "[on grey50] ↳ [/on grey50][grey50]"
+t_skipped_img = "[on grey50] ↳ [/on grey50][grey50]"
+
+t_down_file = "[grey11 on cyan] ↓ [/grey11 on cyan][cyan]"
+t_down_img = "[grey11 on cyan] ↓ [/grey11 on cyan][cyan]"
+
+t_sk = " (skipped)\t  "
+t_dw = " (downloaded)  "
+t_ow = " (overwritten) "
+
+t_tick = "[on grey50]📁✓ [/on grey50][grey50]>"
+t_tick_d = "[grey11 on cyan]📁✓ [/grey11 on cyan][cyan]>"
+t_cross = "[on red]📁× [/on red][red]>"
+
+md5v = " (MD5✓)"
+md5x = " [red](MD5❌)"
+
+# compatible with dark themes only.
